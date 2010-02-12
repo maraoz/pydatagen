d02b005b0dca2825ba6d673e8a70aa909a00e490
manuelaraoz <manuelaraoz@de70215a-162f-11df-8e3c-378b6dd812f8>
2010-02-12 13:10:14 +0000

    added test and header files

---
+++ datagen.py
@@ -0,0 +1,17 @@
+
+
+
+length = len
+lowercase = lowercases = 'abcdefghijklmnopqrstuvwxyz'
+uppercase = uppercases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+letter = letters = lowercase + uppercase
+digit = number = digits = numbers = '0123456789'
+symbol = specialcharacter = symbols = specialcharacters = """~!@#$%^&*()_+|}{":?<>`-=[]\;',./"""
+space = spaces = " "
+characters = symbols+digits+letters
+
+
+
+
+
+
