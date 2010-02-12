a455b773608feb832c4d94bf9c351874daca7d44
manuelaraoz <manuelaraoz@de70215a-162f-11df-8e3c-378b6dd812f8>
2010-02-12 06:40:32 +0000

    Enhancements on GA. Now supports crossover.

--- trunk/ga.py
+++ trunk/ga.py
@@ -5,6 +5,8 @@
 # generic genetic algorithm excecuting class
 
 
+from random import randint, random
+
 class GeneticAlgorithm(object):
     
     def __init__(self, fitness_function, random_chromosome_function,
@@ -31,6 +33,9 @@ class GeneticAlgorithm(object):
         for individual in self.population:
             chrm = individual['genes']
             individual['fitness'] = self.fitness(chrm)
+    def sort_by_fitness(self):
+        cmp_function = lambda c, d: cmp(c['fitness'],d['fitness'])
+        self.population.sort(cmp=cmp_function)
         
     def next_generation(self):
         self.generation += 1
@@ -67,42 +72,80 @@ class GeneticAlgorithm(object):
         best = self.get_best_individual()
         
         return best['genes']
-            
 
-class SimpleGeneticAlgorithm(GeneticAlgorithm):
-    """In this simple version, the offspring we generate are
-        exact copies of the fittest individuals in the population.
-        Then mutation will be applied, so this algorithm simulates
-        asexual reproduction"""
 
-    
+class ElitistGeneticAlgorithm(GeneticAlgorithm):
+    """This class englobes the genetic algorithms which use elitism,
+        that is, that a (usually small) percentage of each population
+        is preserved alive and intact to the next generation. This are
+        the best individuals based on fitness"""
 
     def __init__(self, *args, **kwargs):
 
         elite = 0.4
         GeneticAlgorithm.__init__(self, *args, **kwargs)
 
-
-
-
         # we calculate how many offspring to generate based on the
         # ammount of individuals that will survive to next generation
         self.elite_ammount = int(elite * self.population_size)
         self.offspring_ammount = self.population_size - self.elite_ammount
 
-
-    def get_offspring(self):
-        cmp_function = lambda c, d: cmp(c['fitness'],d['fitness'])
-        self.population.sort(cmp=cmp_function)
-        print "best in gen %d is %s with fitness %d" % \
-              (self.generation, self.population[0]['genes'],
-               self.population[0]['fitness'])
-        return self.population[:self.offspring_ammount]
-
-        
     def introduce(self, offspring):
         assert len(self.population) == self.population_size
         self.population = self.population[:self.elite_ammount] + offspring
+
+    def print_best(ordered = False):
+        if not ordered:
+            best = min(self.population, key = lambda i: i['fitness']
+        else:
+            best = self.population[0]
+        print "best in gen %d is %s with fitness %d" % \
+              (self.generation, best['genes'],best['fitness'])
+
+
+
+class AsexualGeneticAlgorithm(ElitistGeneticAlgorithm):
+    """In this simple version, the offspring we generate are
+        exact copies of the fittest individuals in the population.
+        Then mutation will be applied, so this algorithm simulates
+        asexual reproduction"""
+
+
+    def get_offspring(self):
+        self.sort_by_fitness()
+        self.print_best(ordered=True)
+        return self.population[:self.offspring_ammount]
+
+class CrossoverGeneticAlgorithm(ElitistGeneticAlgorithm):
+    """In this more complex version, the offspring we generate are
+        combination of two parents from previous population. The parents
+        are chosen for mating using a linear roulette wheel based on
+        fitness."""
+
+    def get_random_parent(self, total):
+        r = random() * total
+        s = 0
+        for ind in self.population:
+            s += ind['fitness']
+            if s > r:
+                return ind
+        #shouldn't reach this point
+        raise TypeError, "random number r=%f derived from total=%f \
+            shouldn't be greater than sum s=%f." % (r,total,s)
+
+    def get_offspring(self):
+        self.sort_by_fitness()
+        pop_fitness = sum(i['fitness'] for i in self.population)
+        offspring = []
+        for i in xrange(self.offspring_ammount):
+            father = self.get_random_parent()
+            mother = self.get_random_parent()
+
+            offspring.append(self.combine(father,mother))
+
+        # optional
+        self.print_best(ordered=True)
+        return offspring
         
 
 def main():
